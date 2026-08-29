d8882257d0b2cb1618b78e5f4848b8af6f03d0dd
TemurMalik <tmaq77@gmail.com>
2026-08-29 13:37:44 +0500

    fix(polyfills): restrict window/location polyfills to __DEV__ to prevent production APK startup crash

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,28 +1,30 @@
-// Polyfill global environment objects for React Native / Expo Fast Refresh / HMR
-if (typeof global !== 'undefined') {
-  const defaultLocation = {
-    protocol: 'http:',
-    host: 'localhost:8081',
-    hostname: 'localhost',
-    href: 'http://localhost:8081/',
-    pathname: '/',
-    search: '',
-    origin: 'http://localhost:8081',
-  };
+// Polyfill global environment objects ONLY in DEV mode for Fast Refresh / HMR stability
+if (typeof __DEV__ !== 'undefined' && __DEV__) {
+  if (typeof global !== 'undefined') {
+    const defaultLocation = {
+      protocol: 'http:',
+      host: 'localhost:8081',
+      hostname: 'localhost',
+      href: 'http://localhost:8081/',
+      pathname: '/',
+      search: '',
+      origin: 'http://localhost:8081',
+    };
 
-  if (!global.location) {
-    global.location = defaultLocation;
-  }
-  if (!global.document) {
-    global.document = { currentScript: null };
-  }
-  if (!global.window) {
-    global.window = global;
-  }
-  if (!global.window.location) {
-    global.window.location = defaultLocation;
-  }
-  if (!global.window.document) {
-    global.window.document = global.document;
+    if (!global.location) {
+      global.location = defaultLocation;
+    }
+    if (!global.document) {
+      global.document = { currentScript: null };
+    }
+    if (!global.window) {
+      global.window = global;
+    }
+    if (!global.window.location) {
+      global.window.location = defaultLocation;
+    }
+    if (!global.window.document) {
+      global.window.document = global.document;
+    }
   }
 }
