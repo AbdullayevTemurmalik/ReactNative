46490b8ffc18e2b1f84b1194f9d60cf627efaa41
TemurMalik <tmaq77@gmail.com>
2026-08-30 11:16:40 +0500

    chore(release): bump version to 1.21.9

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -390,7 +390,7 @@ export const ProfileScreen = () => {
         </View>
 
         {/* Versiya */}
-        <Text style={styles.versionText}>SmartBozor Mobile v1.21.8 (Expo SDK 54)</Text>
+        <Text style={styles.versionText}>SmartBozor Mobile v1.21.9 (Expo SDK 54)</Text>
       </ScrollView>
 
       {/* MODALLAR */}
